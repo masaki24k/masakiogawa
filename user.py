44f5df092167ca370e0e79548c049bedefa17d3a
masaki24k <78242764+masaki24k@users.noreply.github.com>
2021-03-29 11:45:50 +0900

    Add files via upload

---
+++ user.py
@@ -0,0 +1,6 @@
+class User(object):
+    def __init__(self, id, username, email, password):
+        self.id = id
+        self.username = username
+        self.email = email
+        self.password = password
